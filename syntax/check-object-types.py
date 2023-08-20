bab9cbb560429670c2abac5347f4e691b0a8725b
ikhrnet <ikhrnet@users.noreply.github.com>
2023-08-21 03:21:14 +0900

    Add files on keywords and types

---
+++ syntax/check-object-types.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+
+import math
+
+print(type(math.pi)) #-> <class 'float'>
+print(type(True))    #-> <class 'bool'>
+
+print(type(math.pi) == float) #-> True
+print(isinstance(True, bool)) #-> True
